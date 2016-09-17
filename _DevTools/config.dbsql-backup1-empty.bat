031679f43182894e452776a0d99334449b3005a3
Davide Gironi <davide.gironi@gmail.com>
2016-09-17 13:15:36 +0200

    updated _DevTools

--- _DevTools/config.dbsql-backup1-empty.bat
+++ _DevTools/config.dbsql-backup1-empty.bat
@@ -1,5 +1,10 @@
 @echo off
 
+rem Backup an MSSQL database to a tsql file, run empty commands
+rem Copyright (c) Davide Gironi, 2014
+
+rem Use this file to customize commands to empty the database
+
 rem perform delete over all tables, exept reports
 %SQLCMD% -S !SQLHOSTNAME! !OPTAUT! -d "!SQLDATABASE!" -Q "EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all'; DECLARE @sql NVARCHAR(max)=''; SELECT @sql += 'DELETE FROM [' + name + ']; ' FROM sys.Tables; EXEC sp_executesql @sql; EXEC sp_msforeachtable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all;'"
 
