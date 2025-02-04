60b6da219cce42c831abd993cb2a711ec6502214
Davide Gironi <davide.gironi@gmail.com>
2025-02-04 19:03:36 +0100

    net40 on vs2022 compile fix

--- DGDataModelSampleModelEntityNETFramework/Model_dgdatamodelsamplemodel.Designer.cs
+++ DGDataModelSampleModelEntityNETFramework/Model_dgdatamodelsamplemodel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Projects\VS\dgdatamodel\DGDataModelSampleModelEntityNETFramework\Model_dgdatamodelsamplemodel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Projects VS\DGDataModel\DGDataModel\DGDataModelSampleModelEntityNETFramework\Model_dgdatamodelsamplemodel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
